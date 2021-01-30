fde042137b85f29900d27ab3d10e3feb543d73dd
avitial <luis.e.avitia@intel.com>
2021-01-29 17:43:45 -0800

    feat: add flag to display frame
    Added boolean (-sf, --show_frame) flag to display frame with OpenCV as the GUI isn't displaying the video correctly. This also helps see the correct metrics (if enabled with -fm, --frame_metrics) in frame.

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ from argparse import ArgumentParser
 from inference import Network
 
 from sys import platform
-# MQTT server environment variables
+
+### MQTT server environment variables
 HOSTNAME = socket.gethostname()
 IPADDRESS = socket.gethostbyname(HOSTNAME)
 MQTT_HOST = IPADDRESS
@@ -63,10 +64,13 @@ def build_argparser():
                              "will look for a suitable plugin for device "
                              "specified (CPU by default)")
     parser.add_argument("-pt", "--prob_threshold", type=float, default=0.5,
-                        help="Probability threshold for detections filtering"
+                        help="Probability threshold for detections filtering "
                         "(0.5 by default)"),
     parser.add_argument("-fm", "--frame_metrics", type=bool, default=False,
-                        help="Enable metrics on displayed frames."
+                        help="Enable print metrics on frames. "
+                        "(False by default)"),
+    parser.add_argument("-sf", "--show_frame", type=bool, default=False,
+                        help="Enable display frames with OpenCV. "
                         "(False by default)")
 
     return parser
@@ -81,10 +85,10 @@ def connect_mqtt():
 
 def preprocess(frame, new_shape, n, c):
     
-    new_frame = cv2.resize(frame, new_shape)
+    new_frame = cv2.resize(frame, (new_shape))
     new_frame = new_frame.transpose((2,0,1))
     new_frame = new_frame.reshape((n, c, new_shape[0], new_shape[1]))
-
+    
     return new_frame
 
 def infer_on_stream(args, client):
@@ -96,7 +100,7 @@ def infer_on_stream(args, client):
     :param client: MQTT client
     :return: None
     """
-    # Vars
+    ### Initialize any class variables desired ###
     ct = 0
     duration = 0
     frame_ct = 0
@@ -104,16 +108,16 @@ def infer_on_stream(args, client):
     predictions = []
     total_ct = 0
 
-    # OpenCV settings
+    ### OpenCV settings ###
     color = (78, 45, 0) 
     font = cv2.FONT_HERSHEY_COMPLEX 
     font_scale = 0.4 
     org = (10, 200) 
     thickness = 1
 
-    # Initialise the class
+    ### Initialise the class ###
     infer_network = Network()
-    # Set Probability threshold for detections
+    ### Set Probability threshold for detections ###
     prob_threshold = args.prob_threshold
 
     ### Load the model through `infer_network` ###
@@ -130,7 +134,7 @@ def infer_on_stream(args, client):
     ### Loop until stream is over ###
     while cap.isOpened():
 
-        detections = 0 ## reset detections in current frame
+        detections = 0 ###reset detections in current frame
         ### Read from the video capture ###
         ret, frame = cap.read()
         if not ret:
@@ -151,7 +155,7 @@ def infer_on_stream(args, client):
             res = infer_network.get_output()
 
             ### Extract any desired stats from the results ###
-            #Draw detections
+            ### Draw detections
             for obj in res[0][0]:
                 class_id, label, conf, xmin, ymin, xmax, ymax = obj
                 if conf >= prob_threshold:
@@ -166,8 +170,8 @@ def infer_on_stream(args, client):
                     bottom_right = (xmax, ymax)
                     color_obj = (0,255,0)
                     frame = cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color_obj, 2)
-                    if not in_frame and not ymin < 75: ## no detection in prev. frame
-                        start_frame = frame_ct ## start counting frames when obj first appears
+                    if not in_frame and not ymin < 75: ###no detection in prev. frame
+                        start_frame = frame_ct ###start counting frames when obj first appears
                         ct = 1
                         total_ct += ct
 
@@ -184,7 +188,7 @@ def infer_on_stream(args, client):
                    duration = frame_ct - start_frame
                    start_frame = frame_ct
                    ct = 0
-                   frames_since_last_ct = 0 ##reset frames since last count
+                   frames_since_last_ct = 0 ###reset frames since last count
             else: 
                 ct = 1
                 frames_since_last_ct = 0
@@ -193,10 +197,11 @@ def infer_on_stream(args, client):
 	        frame = cv2.putText(frame, "People in Frame: {}".format(ct), org, font, font_scale, color, thickness, cv2.LINE_AA)
 	        frame = cv2.putText(frame, "Avg. Duration (in sec): {}".format(duration/10), (org[0],org[1]+15), font, font_scale, color, thickness, cv2.LINE_AA)
 	        frame = cv2.putText(frame, "Total Counted: {}".format(total_ct), (org[0],org[1]+30), font, font_scale, color, thickness, cv2.LINE_AA)
-	        ### Calculate and send relevant information on ###
-            ### current_count, total_count and duration to the MQTT server ###
-            ### Topic "person": keys of "count" and "total" ###
-            ### Topic "person/duration": key of "duration" ###
+	        
+        ### Calculate and send relevant information on ###
+        ### current_count, total_count and duration to the MQTT server ###
+        ### Topic "person": keys of "count" and "total" ###
+        ### Topic "person/duration": key of "duration" ###
         client.publish("person", json.dumps({"count": detections,"total": total_ct}))
         client.publish("person/duration", json.dumps({"duration": duration/10}))
 
@@ -204,12 +209,14 @@ def infer_on_stream(args, client):
         ### Write an output image if `single_image_mode` ###
         sys.stdout.buffer.write(frame)
         sys.stdout.flush()
-        frame = cv2.imshow("Detection Results", frame)
+        
+        if args.show_frame:
+             frame = cv2.imshow("Detection Results", frame)
 
         key = cv2.waitKey(1)
         if key == 27:
             break
-
+    
     cap.release()
     cv2.destroyAllWindows()
     client.disconnect()
@@ -220,11 +227,11 @@ def main():
 
     :return: None
     """
-    # Grab command line args
+    ### Grab command line args ###
     args = build_argparser().parse_args()
-    # Connect to the MQTT server
+    ### Connect to the MQTT server ###
     client = connect_mqtt()
-    # Perform inference on the input stream
+    ### Perform inference on the input stream ###
     infer_on_stream(args, client)
 
 
