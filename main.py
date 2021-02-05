bf1d8349ec32cdd604dd49aeb284166312640fc6
avitial <luis.e.avitia@intel.com>
2021-02-05 11:21:16 -0800

    feat: handling of single image, directory of images and webcam
    Implemented functionality to handle multiple types of inputs and file formats.
       - .mp4 for video files
       - .jpg, .bmp, .png for image file(s)
       - any directory with valid image file(s)
       - cam, webcam or device number (0, 1, 2, etc.) for camera devices

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ import time
 import socket
 import json
 import cv2
-
+import numpy as np 
 import logging as log
 import paho.mqtt.client as mqtt
 
@@ -52,9 +52,9 @@ def build_argparser():
     parser.add_argument("-m", "--model", required=True, type=str,
                         help="Path to an xml file with a trained model.")
     parser.add_argument("-i", "--input", required=True, type=str,
-                        help="Path to image or video file")
+                        help="cam/webcam, path to directory or image or video file.")
     parser.add_argument("-l", "--cpu_extension", required=False, type=str,
-                        default=None,
+                        default="cam",
                         help="MKLDNN (CPU)-targeted custom layers."
                              "Absolute path to a shared library with the"
                              "kernels impl.")
@@ -84,7 +84,7 @@ def connect_mqtt():
     return client
 
 def preprocess(frame, new_shape, n, c):
-    
+    	
     new_frame = cv2.resize(frame, (new_shape))
     new_frame = new_frame.transpose((2,0,1))
     new_frame = new_frame.reshape((n, c, new_shape[0], new_shape[1]))
@@ -107,6 +107,12 @@ def infer_on_stream(args, client):
     in_frame = False
     predictions = []
     total_ct = 0
+    start_frame = 0
+    input_formats = [".png",".bmp",".jpg"]
+    input_type = None
+    image_paths = []
+    image_total = 0
+    i = 0
 
     ### OpenCV settings ###
     color = (78, 45, 0) 
@@ -114,6 +120,8 @@ def infer_on_stream(args, client):
     font_scale = 0.4 
     org = (10, 200) 
     thickness = 1
+    cap = None
+    input_stream = None
 
     ### Initialise the class ###
     infer_network = Network()
@@ -126,14 +134,46 @@ def infer_on_stream(args, client):
     ### Handle the input stream ###
     n, c, h, w = infer_network.get_input_shape()
 
-    input_stream = args.input
-    assert os.path.isfile(args.input), "Specified input file doesn't exist"
-    cap = cv2.VideoCapture(input_stream)
+    try:
+        if args.input == 'cam' or args.input == 'webcam' or args.input == '0': ###if single webcam is available
+            input_type = 'cam'
+            input_stream = 0 ###if RealSense D435 used, set this to 2 for RGB feed
+        elif args.input.endswith('.mp4'): ###check for valid video format
+            if not(os.path.exists(args.input)):
+                assert os.path.isfile(args.input), "Specified input file doesn't exist"
+                return 1
+            input_type = 'vid'
+            input_stream = args.input
+        elif filter(args.input.endswith, input_formats) and not os.path.isdir(args.input): ###check for valid image format
+            if not(os.path.exists(os.path.abspath(args.input))):
+            	assert os.path.isfile(args.input), "Specified input file doesn't exist"
+            	return 1
+            input_type = 'img'
+            input_stream = os.path.abspath(args.input)
+        elif os.path.isdir(args.input) and os.path.exists(args.input):
+            for f in os.listdir(args.input):
+                ext = os.path.splitext(f)[1]
+                if ext.lower() not in input_formats:
+                     continue
+                image_paths.append(os.path.join(os.path.abspath(args.input),f))
+            input_type = 'dir'
+            image_total = len(image_paths) ###set total of valid images found
+            input_stream = image_paths
+        else: 
+            log.error("Specified input file {} doesn't exist".format(args.input))
+    except Exception as e:
+        log.error("ERROR: ", e)
+    
+    if input_type == "dir":
+        cap = cv2.VideoCapture(input_stream[i])
+        i+=1
+    else:
+        cap = cv2.VideoCapture(input_stream)
+    
     print("To close the application, press 'ESC' with focus on the output window")
     
     ### Loop until stream is over ###
-    while cap.isOpened():
-
+    while cap.isOpened() or (input_type == 'dir' and i < image_total):
         detections = 0 ###reset detections in current frame
         ### Read from the video capture ###
         ret, frame = cap.read()
@@ -158,7 +198,7 @@ def infer_on_stream(args, client):
             ### Draw detections
             for obj in res[0][0]:
                 class_id, label, conf, xmin, ymin, xmax, ymax = obj
-                if conf >= prob_threshold:
+                if conf >= prob_threshold and label == 1: ###check only for person detections (coco label=1)
                     detections = 1
                     width = frame.shape[1]
                     height= frame.shape[0]
@@ -171,33 +211,33 @@ def infer_on_stream(args, client):
                     color_obj = (0,255,0)
                     frame = cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color_obj, 2)
                     if not in_frame and not ymin < 75: ###no detection in prev. frame
-                        start_frame = frame_ct ###start counting frames when obj first appears
+                        start_frame = frame_ct ###start frame count when person first appears
                         ct = 1
                         total_ct += ct
 
                     in_frame = True
 
         predictions.append(detections)
-                 
+             
         if in_frame:
 
             if detections == 0:
                 frames_since_last_ct += 1
                 if frames_since_last_ct > 10: 
-                   in_frame = False
-                   duration = frame_ct - start_frame
-                   start_frame = frame_ct
-                   ct = 0
-                   frames_since_last_ct = 0 ###reset frames since last count
+                    in_frame = False
+                    duration = frame_ct - start_frame
+                    start_frame = frame_ct
+                    ct = 0
+                    frames_since_last_ct = 0 ###reset frames since last count
             else: 
                 ct = 1
                 frames_since_last_ct = 0
-        
+    
         if args.frame_metrics:
-	        frame = cv2.putText(frame, "People in Frame: {}".format(ct), org, font, font_scale, color, thickness, cv2.LINE_AA)
-	        frame = cv2.putText(frame, "Avg. Duration (in sec): {}".format(duration/10), (org[0],org[1]+15), font, font_scale, color, thickness, cv2.LINE_AA)
-	        frame = cv2.putText(frame, "Total Counted: {}".format(total_ct), (org[0],org[1]+30), font, font_scale, color, thickness, cv2.LINE_AA)
-	        
+            frame = cv2.putText(frame, "People in Frame: {}".format(ct), org, font, font_scale, color, thickness, cv2.LINE_AA)
+            frame = cv2.putText(frame, "Avg. Duration (in sec): {}".format(duration/10), (org[0],org[1]+15), font, font_scale, color, thickness, cv2.LINE_AA)
+            frame = cv2.putText(frame, "Total Counted: {}".format(total_ct), (org[0],org[1]+30), font, font_scale, color, thickness, cv2.LINE_AA)
+
         ### Calculate and send relevant information on ###
         ### current_count, total_count and duration to the MQTT server ###
         ### Topic "person": keys of "count" and "total" ###
@@ -209,9 +249,27 @@ def infer_on_stream(args, client):
         ### Write an output image if `single_image_mode` ###
         sys.stdout.buffer.write(frame)
         sys.stdout.flush()
-        
-        if args.show_frame:
-             frame = cv2.imshow("Detection Results", frame)
+    
+        if args.show_frame and (input_type == 'img' or input_type == 'dir'):
+            frame = cv2.imshow("Detection Results", frame)
+            cv2.waitKey(1500) ###delay to show single image
+            if input_type == 'dir' and i < image_total:
+                cap = cv2.VideoCapture(input_stream[i])
+                i+=1
+            else:
+                cap.release()
+                return 0
+        elif input_type == 'dir':
+            if i < image_total:
+                cap = cv2.VideoCapture(input_stream[i])
+                i+=1
+            else:
+                cap.release()
+                return 0
+        elif args.show_frame:
+            frame = cv2.imshow("Detection Results", frame)
+        else:
+            continue
 
         key = cv2.waitKey(1)
         if key == 27:
